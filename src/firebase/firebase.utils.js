18a5bb7d128200c4e7f3d04205e3b62a23cfed36
huynhtuan <huynhtuan.wd@gmail.com>
2019-10-22 17:24:57 +0700

    firestore docRef, docSnap

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,6 +13,28 @@ var firebaseConfig = {
     appId: "1:951794836174:web:b65075bde86ed47f7ce69f",
     measurementId: "G-WQ71FPEY9P"
 };
+
+export const createUserProfileDocument = async (userAuth, addtionalData) => {
+    if (!userAuth) return;
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const snapShot = await userRef.get();
+    if (!snapShot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...addtionalData
+            })
+        } catch (error) {
+            console.log('error creating user', error.message);
+        }
+    }
+    return userRef;
+}
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
